0f101c5bf02abb83f31bdadf7b055223e524d4cd
kunalkashyap-1 <kunal.kashyap.8775@gmail.com>
2022-12-10 23:28:12 +0530

    basic list making js

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+let items=[];
+
 const app=express();
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 
 app.get("/",function(req,res){
-    res.render("lists",{var_test:"Test"});
+    let today = new Date();
+
+    let options = {
+        weekday:"long",
+        day:"numeric",
+        month:"long"
+    }
+
+    let day = today.toLocaleDateString("en-US",options);
+    let params = {day:day,
+        items:items};
+
+    res.render("lists",{params});
 });
+
+
+app.post("/",function(req,res){
+    let new_item=req.body.item;
+    items.push(new_item);
+    res.redirect("/");
+});
+
 
 app.listen(8383,()=>{
     console.log("Server running on port 8383");
