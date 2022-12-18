d65cd4320e2962b02a9181b047af6617529d986a
kunalkashyap-1 <kunal.kashyap.8775@gmail.com>
2022-12-18 23:54:59 +0530

    added mongodb with mongoose

--- app.js
+++ app.js
@@ -1,59 +1,104 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
-let items=[];
-let workItems=[];
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://127.0.0.1:27020/todolistDB');
 
-const app=express();
+const listSchema = {
+    name: String
+};
 
-app.use(bodyParser.urlencoded({extended:true}));
+const item = mongoose.model("item", listSchema);
+
+const item1 = new item({
+    name: "Welcome to your ToDo List"
+});
+
+const item2 = new item({
+    name: "<-- Hit this to delete "
+});
+
+const defaultItems = [item1, item2];
+
+const cust_listSchema = {
+    name:String,
+    items:[listSchema]
+};
+
+const list = mongoose.model("list",cust_listSchema);
+
+
+// let items=[];
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
-app.get("/",function(req,res){
+app.get("/", function (req, res) {
     // console.log(date);
     let day = date.get_date();
 
-    let params = {listTitle:day,
-        items:items};
+    item.find({}, function (err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (result.length === 0) {
+                item.insertMany(defaultItems, (err) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log("Data is inserted successfuly");
+                    }
+                });
+                res.redirect("/");
+            }else{
+            let params = {
+                listTitle: day,
+                items: result
+            };
 
-    res.render("lists",{params});
-    
+            res.render("lists", { params });
+        }
+        }
+    })
 });
 
-app.get("/work",function(req,res){
-    
-    let params = {
-        listTitle:"Work List",
-        items:workItems
-    }
-    res.render("lists",{params});
+app.get("/:path",(req,res)=>{
+    const custom_path=req.params.path;
+    const list = new list({
+        name:custom_path,
+        items:defaultItems
+    });
+
+    list.save();
 });
 
-app.get("/about",function(req,res){
-    res.render("about",{});
+
+app.post("/", function (req, res) {
+    let new_item = req.body.item;
+        // items.push(new_item);
+        item.insertMany({
+            name: new_item
+        });
+        res.redirect("/");
 });
 
-app.post("/",function(req,res){
-    let new_item=req.body.item;
-    if(req.body.list === "Work"){
-        workItems.push(new_item);
-        res.redirect("/work");
-    }
-    else{
-    items.push(new_item);
-    res.redirect("/");
-    }
+app.post("/delete",(req,res)=>{
+    // console.log(req.body.checkbox);
+    const checkedItemId=req.body.checkbox;
+    item.findByIdAndRemove(checkedItemId,(err)=>{
+        if(err){
+            console.log(err);
+        }
+        res.redirect("/");
+    });
 });
 
-app.post("/work",function(req,res){
-    let new_item=req.body.item;
-    workItems.push(new_item);
-    res.redirect("/work");
-})
 
-
-app.listen(8383,()=>{
+app.listen(8383, () => {
     console.log("Server running on port 8383");
 });
