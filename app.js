1e607044d5cdbdde1e3177b3758b62ba8c860236
kunalkashyap-1 <kunal.kashyap.8775@gmail.com>
2022-12-19 22:37:48 +0530

    add custom path via express route

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
 const mongoose = require("mongoose");
+const { result } = require("lodash");
+const day = date.get_date();
+const _ = require("lodash");
 
 mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://127.0.0.1:27020/todolistDB');
@@ -10,24 +13,27 @@ const listSchema = {
     name: String
 };
 
-const item = mongoose.model("item", listSchema);
-
-const item1 = new item({
-    name: "Welcome to your ToDo List"
-});
-
-const item2 = new item({
-    name: "<-- Hit this to delete "
-});
-
-const defaultItems = [item1, item2];
-
 const cust_listSchema = {
     name:String,
     items:[listSchema]
 };
 
-const list = mongoose.model("list",cust_listSchema);
+const Item = mongoose.model("item", listSchema);
+const List = mongoose.model("list",cust_listSchema);
+
+
+const item1 = new Item({
+    name: "Welcome to your ToDo List"
+});
+
+const item2 = new Item({
+    name: "<-- Hit this to delete "
+});
+
+const defaultItems = [item1, item2];
+
+
+
 
 
 // let items=[];
@@ -40,14 +46,13 @@ app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
     // console.log(date);
-    let day = date.get_date();
 
-    item.find({}, function (err, result) {
+    Item.find({}, function (err, result) {
         if (err) {
             console.log(err);
         } else {
             if (result.length === 0) {
-                item.insertMany(defaultItems, (err) => {
+                Item.insertMany(defaultItems, (err) => {
                     if (err) {
                         console.log(err);
                     } else {
@@ -68,34 +73,81 @@ app.get("/", function (req, res) {
 });
 
 app.get("/:path",(req,res)=>{
-    const custom_path=req.params.path;
-    const list = new list({
-        name:custom_path,
-        items:defaultItems
+    const custom_path=_.capitalize(req.params.path);
+
+    List.findOne({name:custom_path},(err,result)=>{
+        if(err){
+            console.log(err);
+        }else{
+            if(!result){
+                const list = new List({
+                    name:custom_path,
+                    items:defaultItems
+                });
+            
+                list.save();
+                console.log("new data created in List collection");
+                res.redirect("/"+custom_path);
+            }
+            else{
+                let params = {
+                    listTitle: custom_path,
+                    items: result.items
+                };
+    
+                res.render("lists", { params });
+            }
+        }
     });
 
-    list.save();
+    
 });
 
 
 app.post("/", function (req, res) {
     let new_item = req.body.item;
-        // items.push(new_item);
-        item.insertMany({
+    let listName =req.body.list;
+
+    if(listName == day){
+        Item.insertMany({
             name: new_item
         });
         res.redirect("/");
+    }else{
+        const item = new Item({
+            name:new_item
+        });
+        List.findOne({name:listName},(err,result)=>{
+            result.items.push(item);
+            result.save();
+            res.redirect("/"+listName);
+        });
+    }
+        // items.push(new_item);
+        
 });
 
 app.post("/delete",(req,res)=>{
     // console.log(req.body.checkbox);
     const checkedItemId=req.body.checkbox;
-    item.findByIdAndRemove(checkedItemId,(err)=>{
-        if(err){
-            console.log(err);
-        }
-        res.redirect("/");
-    });
+    const ListName=req.body.list_name;
+
+    if(ListName === day){
+        Item.findByIdAndRemove(checkedItemId,(err)=>{
+            if(err){
+                console.log(err);
+            }
+            res.redirect("/");
+        });
+    }else{
+        List.findOneAndUpdate({name:ListName},{$pull:{items:{_id:checkedItemId}}},(err,result)=>{
+            if(!err){
+                res.redirect("/"+ListName);
+            }
+        });
+    }
+    
+    
 });
 
 
